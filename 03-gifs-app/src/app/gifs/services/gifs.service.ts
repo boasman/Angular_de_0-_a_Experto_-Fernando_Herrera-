0783204f445e303cf45cc8e60aa83cec4052d3fa
elvis michael paulino <epaulino@rauroszm.com>
2025-06-23 16:00:55 +0200

    se creo estructura masonry

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -21,6 +21,17 @@ export class GifService {
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal(true);
 
+
+  tredingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+    for(let i = 0;  i < this.trendingGifs().length;  i += 3){
+      groups.push(this.trendingGifs().slice(i, i+3));
+    }
+
+    console.log(groups);
+    return groups;
+  })
+
   private http = inject(HttpClient);
 
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
