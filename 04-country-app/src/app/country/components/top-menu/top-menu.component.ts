8b9b14607c795a04efe0a19917d2b8ea0ae87621
Elvis Boasman <Elvisboasman@gmail.com>
2025-06-25 12:10:22 +0200

    se pusieron la routas activas en los botones

---
+++ 04-country-app/src/app/country/components/top-menu/top-menu.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports:[RouterLink, RouterLinkActive],
+  selector: 'country-top-menu',
+  templateUrl: './top-menu.component.html',
+  styleUrls: ['./top-menu.component.css']
+})
+export class TopMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- 04-country-app/src/app/layout/CountryLayout/CountryLayout.component.ts
+++ 04-country-app/src/app/layout/CountryLayout/CountryLayout.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopMenuComponent } from "../../country/components/top-menu/top-menu.component";
 
 @Component({
   standalone: true,
-  imports:[RouterOutlet],
+  imports: [RouterOutlet, TopMenuComponent],
   selector: 'app-CountryLayout',
   templateUrl: './CountryLayout.component.html',
   styleUrls: ['./CountryLayout.component.css']
