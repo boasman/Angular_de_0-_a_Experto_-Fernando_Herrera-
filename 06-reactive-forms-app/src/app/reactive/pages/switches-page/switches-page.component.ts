be04d718cf6c1424c3fc04722b586968c65ddc89
Elvis Boasman <Elvisboasman@gmail.com>
2025-07-01 14:35:17 +0200

    voy por el video 5 de la seccion  15

--- 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,18 +1,37 @@
 import { JsonPipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtil } from '../../../utils/form-utils';
 
 @Component({
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   selector: 'app-switches-page',
   templateUrl: './switches-page.component.html',
-  styleUrls: ['./switches-page.component.css']
+  styleUrls: ['./switches-page.component.css'],
 })
 export class SwitchesPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  private fb = inject(FormBuilder);
+
+  formUtil = FormUtil;
+
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotificacion: [true],
+    termAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+   console.log(this.myForm.value);
   }
-
 }
