94d60c1eb1b0545c97656cbbad611afbd893da00
elvis michael paulino <epaulino@rauroszm.com>
2025-07-02 10:36:48 +0200

    preparacion del ejercicios de la seccion 15

--- 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.component.ts
+++ 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.component.ts
@@ -8,7 +8,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { FormUtil } from '../../../utils/form-utils';
-import { FormGroup } from '@angular/forms';
+
 
 @Component({
   standalone: true,
@@ -26,44 +26,52 @@ export class RegisterPageComponent implements OnInit {
 
   formUtils = FormUtil;
 
-  myForm: FormGroup = this.fb.group({
-    name: [
-      '',
-      [Validators.required, Validators.pattern(this.formUtils.namePattern)],
-    ],
-    email: [
-      '',
-      [Validators.required, Validators.pattern(this.formUtils.emailPattern)],
-    ],
-    username: [
-      '',
-      [
-        Validators.required,
-        Validators.minLength(6),
-        Validators.pattern(this.formUtils.notOnlySpacesPattern),
+  myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [Validators.required, Validators.pattern(this.formUtils.namePattern)],
       ],
-    ],
-    password: [
-      '',
-      [Validators.required, Validators.pattern(this.formUtils.passwordPattern)],
-    ],
-    password2: [
-      '',
-      [Validators.required, Validators.pattern(this.formUtils.passwordPattern)],
-    ],
-  });
+      email: [
+        '',
+        [Validators.required, Validators.pattern(this.formUtils.emailPattern)],
+        [this.formUtils.checkingServerResponse]
+      ],
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(this.formUtils.notOnlySpacesPattern),
+          this.formUtils.notStrider
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,Validators.minLength(6)
+
+        ],
+      ],
+      confirPassword: [
+        '',
+        [
+          Validators.required,
+
+        ],
+      ],
+    },
+    {
+      //revisar este metodo
+      Validators: [this.formUtils.isFieldOneEqualFieldTwo('password', 'confirPassword')],
+    }
+  );
 
   onSubmit() {
+    console.log("errores del formulario", this.myForm.errors)
     this.myForm.markAllAsTouched();
     console.log(this.myForm.value);
   }
 
-  isFieldOneEqualFieldTwo(field1: string, field2: string) {
-    return (formGroup: AbstractControl) => {
-      const field1Value = formGroup.get(field1)?.value;
-      const field2Value = formGroup.get(field2)?.value;
 
-      return field1Value === field2Value ? null : { passwordsNotEqual: true };
-    };
-  }
 }
--- 06-reactive-forms-app/src/app/utils/form-utils.ts
+++ 06-reactive-forms-app/src/app/utils/form-utils.ts
@@ -1,4 +1,17 @@
-import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
+
+async function sleep() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 2500);
+  });
+}
 
 export class FormUtil {
   //expressiones regulares
@@ -6,7 +19,8 @@ export class FormUtil {
   static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
   static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
   static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
-  static passwordPattern = /^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).*$/;
+  static passwordPattern =
+    /^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).*$/;
 
   static isValidField(form: FormGroup, fielname: string): boolean | null {
     return form.controls[fielname].errors && form.controls[fielname].touched;
@@ -40,24 +54,79 @@ export class FormUtil {
   static getTextError(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
       switch (key) {
+
         case 'required':
           return 'Este campo es requerido';
+
         case 'minlength':
           return `Minimo de  ${errors['minlength'].requiredLength} caracteres. `;
+
         case 'email':
           return `El correo ingresado no es valido`;
+
         case 'min':
           return `Valor minimo  de  ${errors['min'].min}`;
 
+        case 'emailTaken':
+          return `El correo electronico ya esta siendo usado por otro usuario`;
+
+        case 'notStrider':
+          return `el nombre de strider es protegido`;
+
         case 'pattern':
-          if(errors['pattern'].requiredPattern === FormUtil.emailPattern){
-            return 'El valor ingresado no parece un email'
+          if (errors['pattern'].requiredPattern === FormUtil.emailPattern) {
+            return 'El valor ingresado no parece un email';
           }
           return 'Error de patron contra expression regular';
 
-          default:
-            return `Error de validacion no controlado`;
+        default:
+          return `Error de validacion no controlado`;
       }
+    }
+    return null;
+  }
+
+  // static isFieldOneEqualFieldTwo(field1: string, field2: string) {
+  //   return (formGroup: AbstractControl) => {
+  //     const field1Value = formGroup.get(field1)?.value;
+  //     const field2Value = formGroup.get(field2)?.value;
+
+  //     return field1Value === field2Value ? null : { passwordsNotEqual: true };
+  //   };
+  // }
+
+  static isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { fieldsNotEquals: true };
+    };
+  }
+
+  static async checkingServerResponse(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> {
+    console.log('Validando contra servidor');
+    await sleep(); // 2 segundo medio
+
+    const formValue = control.value;
+
+    if (formValue === 'hola@mundo.com') {
+      return {
+        emailTaken: true,
+      };
+    }
+    return null;
+  }
+
+  static notStrider(control: AbstractControl): ValidationErrors | null {
+    const formValue = control.value;
+
+    if (formValue === 'strider') {
+      return {
+        notStrider: true,
+      };
     }
     return null;
   }
