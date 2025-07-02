9823e2a476fb1980d073f16491cbed2e3e9f4182
elvis michael paulino <epaulino@rauroszm.com>
2025-07-02 09:45:51 +0200

    validar formulario

--- 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.component.ts
+++ 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,12 +1,14 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { FormUtil } from '../../../utils/form-utils';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   standalone: true,
@@ -22,19 +24,46 @@ export class RegisterPageComponent implements OnInit {
 
   private fb = inject(FormBuilder);
 
-  formUtil = FormUtil;
+  formUtils = FormUtil;
 
   myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required, Validators.minLength(6)]],
-    password: ['', [Validators.required, Validators.minLength(0)]],
-    password2: ['', [Validators.required]],
+    name: [
+      '',
+      [Validators.required, Validators.pattern(this.formUtils.namePattern)],
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.pattern(this.formUtils.emailPattern)],
+    ],
+    username: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(this.formUtils.notOnlySpacesPattern),
+      ],
+    ],
+    password: [
+      '',
+      [Validators.required, Validators.pattern(this.formUtils.passwordPattern)],
+    ],
+    password2: [
+      '',
+      [Validators.required, Validators.pattern(this.formUtils.passwordPattern)],
+    ],
   });
 
-
-
   onSubmit() {
+    this.myForm.markAllAsTouched();
     console.log(this.myForm.value);
+  }
+
+  isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { passwordsNotEqual: true };
+    };
   }
 }
--- 06-reactive-forms-app/src/app/utils/form-utils.ts
+++ 06-reactive-forms-app/src/app/utils/form-utils.ts
@@ -1,48 +1,62 @@
 import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtil {
-
   //expressiones regulares
+
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+  static passwordPattern = /^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).*$/;
 
   static isValidField(form: FormGroup, fielname: string): boolean | null {
     return form.controls[fielname].errors && form.controls[fielname].touched;
   }
 
-  static isValidFieldInArray(formArray: FormArray, index: number){
-    return(formArray.controls[index].errors && formArray.controls[index].touched)
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
   }
 
-
   static getFieldError(form: FormGroup, fielname: string): string | null {
-
     if (!form.controls[fielname]) return null;
 
     const errors = form.controls[fielname].errors ?? {};
 
-     return FormUtil.getTextError(errors);
+    return FormUtil.getTextError(errors);
   }
 
-  static getFieldArrayError(formArray: FormArray, index: number): string | null {
-
+  static getFieldArrayError(
+    formArray: FormArray,
+    index: number
+  ): string | null {
     if (formArray.controls.length === 0) return null;
 
     const errors = formArray.controls[index].errors ?? {};
 
     return FormUtil.getTextError(errors);
-
   }
 
-  static getTextError(errors: ValidationErrors){
-
+  static getTextError(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
           return 'Este campo es requerido';
         case 'minlength':
           return `Minimo de  ${errors['minlength'].requiredLength} caracteres. `;
+        case 'email':
+          return `El correo ingresado no es valido`;
         case 'min':
           return `Valor minimo  de  ${errors['min'].min}`;
 
+        case 'pattern':
+          if(errors['pattern'].requiredPattern === FormUtil.emailPattern){
+            return 'El valor ingresado no parece un email'
+          }
+          return 'Error de patron contra expression regular';
+
+          default:
+            return `Error de validacion no controlado`;
       }
     }
     return null;
