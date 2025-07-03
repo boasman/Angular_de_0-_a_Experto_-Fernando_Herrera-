3dbdfb5f9940979e963e49df4ad63974db68bdbd
Elvis Boasman <Elvisboasman@gmail.com>
2025-07-03 10:58:56 +0200

    se configu el archivo correctamente

--- 08-maps-app/scripts/set-envs.js
+++ 08-maps-app/scripts/set-envs.js
@@ -1,28 +1,24 @@
 
-
-const {writeFileSync, mkdirSync} =  require('fs')
-
-
+const { writeFileSync, mkdirSync } = require('fs');
 require('dotenv').config();
 
-const targetPath = './src/environments/environment.ts'
-const targetPathDev = './src/environments/environment.development.ts'
+const targetPath = './src/environments/environment.ts';
+const targetPathDev = './src/environments/environment.development.ts';
 
-const mapboxKey =  process.env['MAPBOX_KEY'];
+const mapboxKey = process.env['MAPBOX_KEY'];
 
-if(!mapboxKey){
-
-  throw new error('MAPBOX_KEY is required');
-
+if (!mapboxKey) {
+    throw new Error('MAPBOX_KEY is required');
 }
 
 const envFileContent = `
-
 export const environment = {
   mapboxKey: "${mapboxKey}"
 };
 `;
 
-mkdirSync('./src/environments', {recursive: true});
-writeFileSync(targetPath,envFileContent);
-writeFileSync(targetPathDev,envFileContent);
+mkdirSync('./src/environments', { recursive: true });
+writeFileSync(targetPath, envFileContent);
+writeFileSync(targetPathDev, envFileContent);
+
+
