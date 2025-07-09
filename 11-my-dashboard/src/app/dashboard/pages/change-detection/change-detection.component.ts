4e73814485853d140c936368f68647c9a40a31ab
elvis michael paulino <epaulino@rauroszm.com>
2025-07-09 10:52:09 +0200

    Se termino la parte de change Detection

--- 11-my-dashboard/src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ 11-my-dashboard/src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,17 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports:[],
+  imports: [TitleComponent, JsonPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-change-detection',
-  templateUrl: './change-detection.component.html',
+  template: `
+    <app-title [title]="currentFrameWork()"></app-title>
 
+    <pre>{{ frameworkAsProperties | json }}</pre>
+
+    <pre>{{ frameworkAsSignal() | json }}</pre>
+  `,
 })
-export default class ChangeDetectionComponent implements OnInit {
+export default class ChangeDetectionComponent {
+  public currentFrameWork = computed(
+    () => `Change Detection - ${this.frameworkAsSignal().name}`
+  );
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      //this.frameworkAsProperties.name  = 'React'
 
-  ngOnInit() {
+      this.frameworkAsSignal.update((valor) => ({
+        ...valor,
+        name: 'React',
+      }));
+
+      console.log('Hecho');
+    }, 3000);
   }
 
+  frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  frameworkAsProperties = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
 }
